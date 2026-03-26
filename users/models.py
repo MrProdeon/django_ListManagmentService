a5162fdb6e9d17677ce5952c0478e9bf4c8f87a6
Prodeon <prodeon21@gmail.com>
2026-03-26 10:58:16 +0300

    delete email from REQUIRED_FIELDS

--- users/models.py
+++ users/models.py
@@ -7,14 +7,15 @@ class CustomUser(AbstractUser):
     last_name = None
     username = None
 
-    full_name = models.CharField(150, verbose_name="Ф.И.О.")
-    phone_number = models.CharField(15, verbose_name="Номер телефона")
+    full_name = models.CharField(max_length=150, verbose_name="Ф.И.О.")
+    phone_number = models.CharField(max_length=15, verbose_name="Номер телефона")
     comment = models.TextField(blank=True, verbose_name="Комментарий")
     avatar = models.ImageField(blank=True, null=True, verbose_name="Аватар")
-    country = models.CharField(50, verbose_name="Страна")
+    country = models.CharField(max_length=50, verbose_name="Страна")
 
     email = models.EmailField(unique=True, verbose_name="Электронная почта")
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
