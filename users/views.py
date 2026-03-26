5eb2a43c426b01feb0bf4b0f532d7554e0b09f9e
Prodeon <prodeon21@gmail.com>
2026-03-26 12:08:26 +0300

    add views for delete and read

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DeleteView
 from users.models import CustomUser
 from users.forms import UserForm
 
@@ -10,4 +10,15 @@ class CreateUser(CreateView):
     model = CustomUser
     form_class = UserForm
     template_name = "create_or_update_user.html"
-    success_url = reverse_lazy("list_users")
+    success_url = reverse_lazy("users:list_users")
+
+class ListUser(ListView):
+    model = CustomUser
+    template_name = "list_user.html"
+    context_object_name = "users"
+
+class DeleteUser(DeleteView):
+    model = CustomUser
+    template_name = "confirm_delete.html"
+    success_url = reverse_lazy("users:list_user")
+    context_object_name = "user"
