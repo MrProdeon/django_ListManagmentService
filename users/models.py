657c7e7a6e1705ad2541d1b2878bc3a7cc6cacb5
Prodeon <prodeon21@gmail.com>
2026-03-26 10:50:33 +0300

    add phone_number, avatar and country in CustomUser

--- users/models.py
+++ users/models.py
@@ -8,7 +8,10 @@ class CustomUser(AbstractUser):
     username = None
 
     full_name = models.CharField(150, verbose_name="Ф.И.О.")
+    phone_number = models.CharField(15, verbose_name="Номер телефона")
     comment = models.TextField(blank=True, verbose_name="Комментарий")
+    avatar = models.ImageField(blank=True, null=True, verbose_name="Аватар")
+    country = models.CharField(50, verbose_name="Страна")
 
     email = models.EmailField(unique=True, verbose_name="Электронная почта")
     USERNAME_FIELD = "email"
@@ -19,3 +22,4 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
