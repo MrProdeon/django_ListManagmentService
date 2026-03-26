04a0cd2921b2a1e0c40de5109cd4b291639985ba
Prodeon <prodeon21@gmail.com>
2026-03-26 12:08:06 +0300

    add all urls for CRUD

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from users.views import CreateUser
+from users.views import CreateUser, ListUser, DeleteUser
 
 app_name = "users"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("create_user/", CreateUser.as_view(), name="create_user")
+    path("create_user/", CreateUser.as_view(), name="create_user"),
+    path("list_user/", ListUser.as_view(), name="list_user"),
+    path("update_user/<int:pk>/", CreateUser.as_view(), name="update_user"),
+    path("delete_user/<int:pk>/", DeleteUser.as_view(), name="delete_user")
 ]
