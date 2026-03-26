21226f3014aba82ba523d6bc1650a6d8a6ed3d69
Prodeon <prodeon21@gmail.com>
2026-03-26 11:47:27 +0300

    add CreateUser view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from users.models import CustomUser
+
 
 # Create your views here.
+
+class CreateUser(CreateView):
+    model = CustomUser
+    fields = ["full_name", "phone_number", "country", "email", "avatar"]
+    template_name = "create_or_update_user.html"
+    success_url = reverse_lazy("list_users")
