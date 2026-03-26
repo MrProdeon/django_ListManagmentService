46be38d882ae23d924aa6515d261d21721215458
Prodeon <prodeon21@gmail.com>
2026-03-26 11:54:55 +0300

    delete fields from view

--- users/views.py
+++ users/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from users.models import CustomUser
-
+from users.forms import UserForm
 
 # Create your views here.
 
 class CreateUser(CreateView):
     model = CustomUser
-    fields = ["full_name", "phone_number", "country", "email", "avatar"]
+    form_class = UserForm
     template_name = "create_or_update_user.html"
     success_url = reverse_lazy("list_users")
