1ddafabcf1206dbd2665915d5b085d42ee108562
Prodeon <prodeon21@gmail.com>
2026-03-28 07:55:39 +0300

    create model for messages and migrated

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MessageModel(models.Model):
+
+    subject_line = models.CharField(max_length=255, blank=True, null=True, verbose_name="Тема письма")
+    message_text = models.TextField(verbose_name="Текст сообщения")
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
