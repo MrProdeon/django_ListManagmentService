c2a7408097becbb4ea6ee0744ba245925fcaf479
Prodeon <prodeon21@gmail.com>
2026-03-29 11:11:43 +0300

    add url and view for MessageUpdateView

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from mailing.views import MessageCreateView, MessageListView
+from mailing.views import MessageCreateView, MessageListView, MessageUpdateView
 
 app_name = "mailing"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", MessageListView.as_view(), name="list_message"),
-    path("create/", MessageCreateView.as_view(), name="create_message")
+    path("create/", MessageCreateView.as_view(), name="create_message"),
+    path("update/<int:pk>", MessageUpdateView.as_view(), name="update_message")
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView
 from mailing.models import MessageModel
 from mailing.forms import MessageCreateForm
 
@@ -9,9 +10,19 @@ class MessageCreateView(CreateView):
     model = MessageModel
     template_name = "create_message.html"
     form_class = MessageCreateForm
-    success_url = 0
+    success_url = reverse_lazy("mailing:list_message")
+    context_object_name = "message"
+
+class MessageUpdateView(UpdateView):
+    model = MessageModel
+    template_name = "create_message.html"
+    form_class = MessageCreateForm
+    success_url = reverse_lazy("mailing:list_message")
+    context_object_name = "message"
 
 class MessageListView(ListView):
     model = MessageModel
     template_name = "list_message.html"
     context_object_name = "messages"
+
+class MessageDetailView
