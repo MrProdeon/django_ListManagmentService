55e05fca656c942e8916ceb095a6dad56bd9d645
Prodeon <prodeon21@gmail.com>
2026-03-29 10:49:59 +0300

    add create and list view for messages, create url for it

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from mailing.views import MessageCreateView, MessageListView
 
 app_name = "mailing"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
+    path("", MessageListView.as_view(), name="list_message"),
+    path("create/", MessageCreateView.as_view(), name="create_message")
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from mailing.models import MessageModel
+from mailing.forms import MessageCreateForm
+
 
 # Create your views here.
+class MessageCreateView(CreateView):
+    model = MessageModel
+    template_name = "create_message.html"
+    form_class = MessageCreateForm
+    success_url = 0
+
+class MessageListView(ListView):
+    model = MessageModel
+    template_name = "list_message.html"
+    context_object_name = "messages"
